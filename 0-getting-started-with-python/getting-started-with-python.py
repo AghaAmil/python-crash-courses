2ba091109310338b96ee3cf1d4c4b7eee11a93af
Amirhossein Moravveji <aghaamil@Amirhosseins-MacBook-Pro.local>
2023-10-06 15:58:03 +0330

    changing comment styles

--- 0-getting-started-with-python/getting-started-with-python.py
+++ 0-getting-started-with-python/getting-started-with-python.py
@@ -1,28 +1,36 @@
-# Lesson 1 - Memory and Variable
-# Lesson Takeaway
+"""
+Lesson 1 - Memory and Variable
+Lesson Takeaway
 
-# Computer programs use variables to remember important information
-# A variable has a name and a value
-# You can create a variable by connecting the name and the value with an equal sign =
+- Computer programs use variables to remember important information
+- A variable has a name and a value
+- You can create a variable by connecting the name and the value with an equal sign =
+"""
 
 # Let's create some variables
 name = 'Amirhossein'
 lastName = 'Moravveji'
 city = 'Tehran'
 
-
-
-# Lesson 2 - Text Data
-# Lesson Takeaway
+"""
+Lesson 2 - Text Data
+Lesson Takeaway
 
 #  A piece of text is called a string
 # Strings require quotation marks
 # The print() statement is used to send a value to the screen
+"""
 
 # I know this code to display the variables
 print(name)
 print(lastName)
 print(city)
 
+"""
+# Lesson 3 - Numerical Numbers
+# Lesson Takeaway
 
-
+# Numerical values can be stored in variables
+# You can access the value stored in a variable by calling its name
+# Numerical data should not be surrounded by quotation marks
+"""
