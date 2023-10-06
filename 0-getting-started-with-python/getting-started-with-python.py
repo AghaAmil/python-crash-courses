62744ae5526329cc99d333fb77c0ef4f6eefc16b
Amirhossein Moravveji <aghaamil@Amirhosseins-MacBook-Pro.local>
2023-10-06 15:33:20 +0330

    text data lesson

--- 0-getting-started-with-python/getting-started-with-python.py
+++ 0-getting-started-with-python/getting-started-with-python.py
@@ -1,3 +1,4 @@
+# Lesson 1 - Memory and Variable
 # Lesson Takeaway
 
 # Computer programs use variables to remember important information
@@ -5,11 +6,23 @@
 # You can create a variable by connecting the name and the value with an equal sign =
 
 # Let's create some variables
-name = 'amirhossein'
-lastName = 'moravveji'
+name = 'Amirhossein'
+lastName = 'Moravveji'
+city = 'Tehran'
 
+
+
+# Lesson 2 - Text Data
+# Lesson Takeaway
+
+#  A piece of text is called a string
+# Strings require quotation marks
+# The print() statement is used to send a value to the screen
 
 # I know this code to display the variables
 print(name)
 print(lastName)
+print(city)
+
+
 
