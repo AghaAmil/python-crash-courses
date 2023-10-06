0a1ed8f578abe50b3d67ca2d068e776be30ae8fc
Amirhossein Moravveji <aghaamil@Amirhosseins-MacBook-Pro.local>
2023-10-06 17:07:18 +0330

    debugging lesson

---
+++ 1-going-deeper-with-python/going-deeper-with-python.py
@@ -0,0 +1,11 @@
+"""
+Lesson 1 - Debugging
+Lesson Takeaways
+
+- Errors in code are known as bugs
+- Code is executed line by line from top to bottom
+- Code execution is interrupted by bugs
+"""
+
+# The practices already mentioned in the sololearn website
+# Nothing to add
