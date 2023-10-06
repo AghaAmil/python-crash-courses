0f08f2b89b0e17bc7c65ed5d20536a2ba053bf64
Amirhossein Moravveji <aghaamil@Amirhosseins-MacBook-Pro.local>
2023-10-06 15:07:43 +0330

    memory and variable lesson added

---
+++ 0-getting-started-with-python/memory-and-variable.py
@@ -0,0 +1,15 @@
+# Lesson Takeaway
+
+# Computer programs use variables to remember important information
+# A variable has a name and a value
+# You can create a variable by connecting the name and the value with an equal sign =
+
+# Let's create some variables
+name = 'amirhossein'
+lastName = 'moravveji'
+
+
+# I know this code to display the variables
+print(name)
+print(lastName)
+
