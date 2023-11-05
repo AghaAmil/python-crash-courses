033db16810efdf0732bbcfd3ab241900800cf65c
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-05 16:10:36 +0330

    adding functions challenges

--- 5-functions/functions-challenges.py
+++ 5-functions/functions-challenges.py
@@ -2,9 +2,28 @@
 This function takes a long list as an argument and tells the numbers in list are odd or even and categorize them
 into a new list
 """
-list_of_nums = [3, 4, 972, 48, 27, 297, 83, 15, 37, 93, 74, 67, 98, 30, 10, 973, 3810, 973, 28, 34, 74, 93, 82, 23, 1]
-print(list_of_nums)
+# list_of_nums = [3, 4, 972, 48, 27, 297, 83, 15, 37, 93, 74, 67, 98, 30, 10, 973, 3810, 973, 28, 34, 74, 93, 82, 23, 1]
+# print(list_of_nums)
+print('This function gets a list of numbers and categories them into odd and even numbers')
+nums_list_str = input("Enter preferred list of numbers and separate them by space: ")
+nums_list = nums_list_str.split()
+print(nums_list)
 
+
+def divider(input):
+    even_nums = []
+    odd_nums = []
+    for i in range(len(input)):
+        num = int(input[i])
+        if num % 2 == 0:
+            even_nums.append(num)
+        else:
+            odd_nums.append(num)
+    print('This is the list of even numbers: ', even_nums)
+    print('This is the list of odd numbers: ', odd_nums)
+
+
+divider(nums_list)
 """
 The given code takes a text and a word as input and passes them to a function called search().
 The search() function should return "Word found" if the word is present in the text, or "Word not found", if it’s not.
