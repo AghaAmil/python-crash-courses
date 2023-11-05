1749fd3aa9b6ec4768f3ee951143ceac8b702c40
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-05 20:30:16 +0330

    tuples lesson

--- 6-collection-types/collection-type.py
+++ 6-collection-types/collection-type.py
@@ -51,3 +51,52 @@ country_data = {
 
 country = input('Enter the country\'s name: ')
 print(country_data.get(country, 'Not found'))
+
+"""
+Lesson 3 - Tuples
+"""
+
+names = ('amir', 'ali', 'hasan', 'asgar')
+names1 = 'akbar', 'asghar', 'ahmad', 'antar'  # without parentheses
+names2 = 1, (1, 2, 3), 2, 3, 'pashm'  # nested tuples
+
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+print(names1)
+print(names2)
+
+"""
+You are given a list of contacts, where each contact is represented by a tuple, with the name and age of the
+contact. Complete the program to get a string as input, search for the name in the list of contacts and output the
+age of the contact in the format presented below:
+
+Sample Input
+John
+
+Sample Output
+John is 31
+"""
+
+contacts = [
+    ('James', 42),
+    ('Amy', 24),
+    ('John', 31),
+    ('Amanda', 63),
+    ('Bob', 18)
+]
+
+contact_name = input('Enter the name of the person in the contacts to get the details: ')
+result = ''
+
+for i in range(len(contacts)):
+    if contact_name in contacts[i][0]:
+        result = (str(contacts[i][0]) + ' is ' + str(contacts[i][1]))
+        break
+    else:
+        result = "Not Found"
+
+print(result)
