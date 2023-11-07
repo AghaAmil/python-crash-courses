e90868e29b6a890a857579e75b349ce51fe67d1b
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-07 21:17:53 +0330

    sets challenges

--- 6-collection-types/collection-type-challenges.py
+++ 6-collection-types/collection-type-challenges.py
@@ -1,1 +1,21 @@
-# Challenges will be added later
+"""
+You are working on a recruitment platform, which should match the available jobs and the candidates based on their
+skills. The skills required for the job, and the candidate's skills are stored in sets. Complete the program to
+output the matched skill.
+
+Note: Display the result as string
+
+"""
+
+skills = {'Python', 'HTML', 'CSS', 'SQL', 'C++', 'Java', 'Scala', 'C#'}
+job_skills = {'HTML', 'CSS', 'JS', 'C#', 'NodeJS'}
+
+matched_skills = skills & job_skills
+print(matched_skills)
+
+matched_skills_str = ''
+
+for i in matched_skills:
+    matched_skills_str += i
+
+print(matched_skills_str)
