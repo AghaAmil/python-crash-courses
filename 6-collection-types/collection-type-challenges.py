2885ccd0ed55acb466df9e366f135091f4d9feaa
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-08 20:06:18 +0330

    list comprehension lesson

--- 6-collection-types/collection-type-challenges.py
+++ 6-collection-types/collection-type-challenges.py
@@ -19,3 +19,26 @@ for i in matched_skills:
     matched_skills_str += i
 
 print(matched_skills_str)
+
+"""
+Given a word as input, output a list, containing only the letters of the word that are not vowels.
+The vowels are a, e, i, o, u. 
+
+Sample Input
+awesome
+
+Sample Output 
+['w', 's', 'm'] 
+"""
+
+# That how noobs do it:
+
+# input_word = input('Enter your preferred word: ')
+# output = [i for i in input_word if i != 'a' if i != 'e' if i != 'i' if i != 'o' if i != 'u']
+# print(output)
+
+input_word = input('Enter your preferred word: ')
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+output_word = [i for i in input_word if i not in vowels]
+print(output_word)
