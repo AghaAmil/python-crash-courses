7ca975914a2fcec0135b36509b0f8b2e0d3ecfca
AghaAmil <moravveji.amir.92@gmail.com>
2023-11-09 20:31:18 +0330

    adding map & filter challenges

--- 7-functional-programming/functional-programming-challenges.py
+++ 7-functional-programming/functional-programming-challenges.py
@@ -14,3 +14,16 @@ discount_percentage = int(input('Enter the discount percentage: '))
 new_price = (lambda a, b: a - (a * b / 100))(price, discount_percentage)
 
 print('The item price after the discount is: ', new_price)
+
+"""
+You work on a payroll program.
+Given a list of salaries, you need to take the bonus (in percent) everybody is getting as input
+and increase all the salaries by that amount. Output the resulting list.
+"""
+
+salaries = [2000, 1800, 3100, 4400, 1500]
+bonus = int(input('Enter the bonus percentage: '))
+
+salary_cal = list(map(lambda x: int(x + (x * bonus / 100)), salaries))
+
+print(salary_cal)
