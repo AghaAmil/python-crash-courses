845b6a33b3d5d69eabc90c14e0cb8e617c052987
AghaAmil <moravveji.amir.92@gmail.com>
2023-11-13 21:58:33 +0330

    inheritance challenge added

--- 8-object-oriented-programming/oop-challenges.py
+++ 8-object-oriented-programming/oop-challenges.py
@@ -28,3 +28,33 @@ player_level = (input('Enter\'s the player level: '))
 
 gamer_1 = Player(player_name, player_level)
 gamer_1.intro()
+
+"""
+You are making a drawing application, which has a Shape base class.
+The given code defines a Rectangle class, creates a Rectangle object and calls its area() and perimeter() methods.
+Do the following to complete the program:
+1. Inherit the Rectangle class from Shape.
+2. Define the perimeter() method in the Rectangle class, printing the perimeter of the rectangle.
+"""
+
+
+class Shape:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        print(self.width * self.height)
+
+
+class Rectangle(Shape):
+    def perimeter(self):
+        print(2 * (self.width + self.height))
+
+
+shape_width = int(input('\nEnter the width of the shape: '))
+shape_height = int(input('Enter the height of the shape: '))
+
+shape_num_1 = Rectangle(shape_width, shape_height)
+shape_num_1.area()
+shape_num_1.perimeter()
