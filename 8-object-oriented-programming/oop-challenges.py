da85ea7ff660af832ced6f5d8a65645ac5e6ef07
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-14 19:58:25 +0330

    magic methods & operator overloading challenge added

--- 8-object-oriented-programming/oop-challenges.py
+++ 8-object-oriented-programming/oop-challenges.py
@@ -58,3 +58,43 @@ shape_height = int(input('Enter the height of the shape: '))
 shape_num_1 = Rectangle(shape_width, shape_height)
 shape_num_1.area()
 shape_num_1.perimeter()
+
+"""
+We are improving our drawing application.
+Our application needs to support adding and comparing two Shape objects.
+Add the corresponding methods to enable addition + and comparison using the greater than > operator for the Shape class.
+The addition should return a new object with the sum of the widths and heights of the operands, while the comparison
+should return the result of comparing the areas of the objects.
+"""
+
+
+class Shape:
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def area(self):
+        return self.width * self.height
+
+    # your code goes here
+    def __add__(self, other):
+        return Shape(self.width + other.width, self.height + other.height)
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+
+w1 = int(input())
+h1 = int(input())
+w2 = int(input())
+h2 = int(input())
+
+s1 = Shape(w1, h1)
+s2 = Shape(w2, h2)
+result = s1 + s2
+
+print(
+    '\nThe given code creates two Shape objects from user input, outputs the area() of their addition and compares '
+    'them.')
+print(result.area())
+print(s1 > s2)
