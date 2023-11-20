b96cd0ac0752b6a053144fbcb562289773413aa8
AghaAmil <moravveji.amir.92@gmail.com>
2023-11-20 21:00:27 +0330

    data hidding challenge added

--- 8-object-oriented-programming/oop-challenges.py
+++ 8-object-oriented-programming/oop-challenges.py
@@ -98,3 +98,31 @@ print(
     'them.')
 print(result.area())
 print(s1 > s2)
+
+"""
+We are working on a game. Our Player class has name and private _lives attributes. The hit() method should
+decrease the lives of the player by 1. In case the lives equal to 0, it should output "Game Over". Complete the hit()
+method to make the program work as expected.
+"""
+
+
+class Player:
+    def __init__(self, name, lives):
+        self.name = name
+        self._lives = lives
+
+    def hit(self):
+        self._lives -= 1
+
+        if self._lives == 0:
+            print('Game Over')
+
+
+p1 = Player('Cyberpunk77', 3)
+
+p1.hit()
+p1.hit()
+
+print(p1.name, p1._lives)
+
+p1.hit()
