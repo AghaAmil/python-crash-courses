d9c71835292df768852028edfa82c2c319758687
AghaAmil <moravveji.amir.92@gmail.com>
2023-11-20 21:58:04 +0330

    class and static methods challenge added

--- 8-object-oriented-programming/oop-challenges.py
+++ 8-object-oriented-programming/oop-challenges.py
@@ -126,3 +126,26 @@ p1.hit()
 print(p1.name, p1._lives)
 
 p1.hit()
+
+print('')
+"""
+The given code takes 2 numbers as input and calls the static area() method of the Shape class, to output the area
+of the shape, which is equal to the height multiplied by the width. To make the code work, you need to define the
+Shape class, and the static area() method, which should return the multiplication of its two arguments.
+"""
+
+
+class Shape:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @staticmethod
+    def area(width, height):
+        return width * height
+
+
+w = int(input('Enter the width of the shape: '))
+h = int(input('Enter the height of the shape: '))
+
+print(Shape.area(w, h))
