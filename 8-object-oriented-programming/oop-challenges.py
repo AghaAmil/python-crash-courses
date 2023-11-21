6380d668ad543729f84f6511afd98e1aa01e8f37
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-21 20:03:05 +0330

    properties challenge added

--- 8-object-oriented-programming/oop-challenges.py
+++ 8-object-oriented-programming/oop-challenges.py
@@ -149,3 +149,34 @@ w = int(input('Enter the width of the shape: '))
 h = int(input('Enter the height of the shape: '))
 
 print(Shape.area(w, h))
+
+"""
+We are improving our game and need to add an isAlive property, which returns True if the lives count is greater than 0.
+Complete the code by adding the isAlive property.
+"""
+
+
+class Player:
+    def __init__(self, name, lives):
+        self.name = name
+        self._lives = lives
+
+    def hit(self):
+        self._lives -= 1
+
+    # your code goes here
+    @property
+    def isAlive(self):
+        if self._lives > 0:
+            return True
+
+
+p = Player("Cyberpunk77", int(input()))
+i = 1
+while True:
+    p.hit()
+    print("Hit # " + str(i))
+    i += 1
+    if not p.isAlive:
+        print("Game Over")
+        break
