ac79a55f8050d077e8ea11aa0c520c54eaa5a7a9
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-21 21:27:26 +0330

    exception lessons

---
+++ 9-exceptions/exceptions.py
@@ -0,0 +1,26 @@
+"""
+Lesson 1 - Exceptions
+
+An exception is an event, which occurs during the execution of a program that disrupts the normal flow of the program.
+"""
+
+# ZeroDivisionError
+num1 = 19
+num2 = 0
+# print(num1 / num2)    # ZeroDivisionError: division by zero
+
+"""
+Different exceptions are raised for different reasons. 
+Common exceptions:
+
+ImportError: an import fails;
+IndexError: a list is indexed with an out-of-range number;
+NameError: an unknown variable is used;
+SyntaxError: the code can't be parsed properly; 
+TypeError: a function is called on a value of an inappropriate type;
+ValueError: a function is called on a value of the correct type, but with an inappropriate value.
+"""
+
+# print('7' + 4)        # TypeError is raised
+
+
