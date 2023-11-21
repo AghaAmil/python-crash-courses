3e5a1d9eaf3afe26e128b34ea510f03bc2ce2230
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-11-21 21:29:33 +0330

    exception handling lesson

--- 9-exceptions/exceptions.py
+++ 9-exceptions/exceptions.py
@@ -23,4 +23,53 @@ ValueError: a function is called on a value of the correct type, but with an ina
 
 # print('7' + 4)        # TypeError is raised
 
+"""
+Lesson 2 - Exception Handling
+"""
+try:
+    print(num1 / num2)
+    print('Calculation is done')
+except ZeroDivisionError:
+    print('An error Occurred')
+    print('Due to zero division')
+
+print('')
+# multiple exception handling
+
+try:
+    var = 10
+    print(var + 'hello')
+    print(var / 2)
+except ZeroDivisionError:
+    print('Faced a zero division error')
+except (TypeError, ValueError):
+    print('Type or Value Occurred')
+
+print('')
+# no exception defined
+
+try:
+    word = 'akbar'
+    print(word / 0)
+except:
+    print('Faced an error')
+
+"""
+An ATM machine takes the amount to be withdrawn as input and calls the corresponding withdrawal method.
+In case the input is not a number, the machine should output "Please enter a number".
+Use exception handling to take a number as input, call the withdraw() method with the input as its argument, 
+and output "Please enter a number", in case the input is not a number.
+"""
+
+
+def withdraw(amount):
+    print(str(amount) + " withdrawn!")
+
+
+# your code goes here
+try:
+    amount = int(input('Enter the amount of withdraw: '))
+    withdraw(amount)
+except ValueError:
+    print('Please enter a number')
 
