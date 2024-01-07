771e3e0d9e44f71bf00b0a1373405ba3ee6e1297
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-01-07 12:18:24 +0330

    review the python functions

---
+++ 5-functions/reviewing-functions.py
@@ -0,0 +1,12 @@
+# python built-in functions
+
+var1 = range(3, 22, 3)
+print(list(var1))
+
+var2 = str(23.1)
+print(var2)
+
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+print(letters.index('r'))
+print(letters.index('p'))
+print(letters.index('q'))
