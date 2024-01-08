3104823dc7d0f21826eae1e2cad6c10f4cadab4f
AghaAmil <moravveji.amir.92@gmail.com>
2024-01-08 21:42:39 +0330

    encapsulation in python

--- 8-object-oriented-programming/review-oop/python-oops-concepts.py
+++ 8-object-oriented-programming/review-oop/python-oops-concepts.py
@@ -96,7 +96,7 @@ guest.details()
 print('\n')
 
 """
-Polymorphism simply means having many forms.
+Polymorphism in Python
 """
 
 print('This code demonstrates the concept of inheritance and method overriding in Python classes')
@@ -140,4 +140,46 @@ ostrich_1 = Ostrich()
 ostrich_1.intro()
 ostrich_1.flight()
 
+# blank lines
+print('\n')
 
+"""
+Encapsulation in Python
+"""
+
+print('Encapsulation puts restrictions on accessing variables and methods directly and can prevent the accidental '
+      'modification of data.')
+print('-----------------------------------------------------------------------------------------------------------'
+      '---------------------')
+
+
+# creating a parent class with a private variable
+class Base:
+
+    def __init__(self):
+        self.var1 = 'not a private variable'
+        self.__var2 = 'private variable'
+
+    @property
+    def var2(self):
+        return self._var2
+
+
+# creating a child class
+class Derived(Base):
+
+    def __init__(self):
+        # calling the constructor of the Base class
+        Base.__init__(self)
+        print('Calling the private variable of the Base class below:')
+        print(self.__var2)
+
+
+object_1 = Base()
+print(object_1.var1)
+
+# does not display the private variable
+object_2 = Derived()
+
+# will cause an error
+# print(object_1.__var2)
