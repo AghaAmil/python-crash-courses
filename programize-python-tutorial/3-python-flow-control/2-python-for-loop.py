e48872a8745b07a798b84ff88473d5e72660f2b7
AghaAmil <moravveji.amir.92@gmail.com>
2024-10-08 18:46:45 +0330

    python for loop

---
+++ programize-python-tutorial/3-python-flow-control/2-python-for-loop.py
@@ -0,0 +1,22 @@
+# Loop Through a lists
+languages = ['Python', 'Java', 'JavaScript', 'Go', 'Swift']
+
+for language in languages:
+    print('This is a programming language: ', language)
+
+# blank line
+print()
+
+# Loop Through a String
+sample_string = 'Amirhossein Moravveji'
+
+for char in sample_string:
+    print(char, end=' ')
+
+# blank line
+print('\n')
+
+# Loop using Range() Function
+for i in range(5):
+    print(i, end=' ')
+
